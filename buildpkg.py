e79a75e785a3529710a54c9638206648933d0055
流光 <1004383796@qq.com>
2018-09-12 18:29:17 +0800

    [更新] 更新生成scons脚本功能.

--- buildpkg.py
+++ buildpkg.py
@@ -3,7 +3,7 @@
 # @Author: liu2guang
 # @Date:   2018-09-11 18:07:00
 #
-# @LICENSE: https://github.com/liu2guang/buildpkg/blob/master/LICENSE.
+# @LICENSE: https://github.com/rtpkgs/buildpkg/blob/master/LICENSE.
 #
 # Change Logs:
 # Date           Author       Notes
@@ -13,9 +13,10 @@ import os, sys
 import argparse
 import shutil
 import stat
+import platform 
 
 # 模板仓库地址
-rtthread_pkg_example_repo = "https://github.com/liu2guang/rtthread_pkg_example.git"
+rtthread_pkg_example = "https://github.com/rtpkgs/rtthread_pkg_example.git" 
 
 # 声明命令
 # pkgname
@@ -23,52 +24,110 @@ parser = argparse.ArgumentParser(
     description = 'Quick build rt-thread pkg toolkits')
 parser.add_argument('pkgname', type = str, help = 'The package name that needs to be created') 
 parser.add_argument('-u', '--url', type = str, help = 'The URL needs to be build into package') 
+parser.add_argument('-l', '--license', type = str, help = 'The license for package') 
+parser.add_argument('-v', '--version', type = str, help = 'The license for version') 
 
-# 创建工程
-def buildpkg_mkdir(args):
+# 生成工程
+def buildpkg_make_project(pkgname, url):
+
+    print("生成工程中...")
 
     # 检查是否工程已经存在
-    if os.path.exists(args.pkgname) == True:
-
-        # Todo: 这里可以针对已经存在的包名做一个备份处理, 让用户体验感提示!
+    if os.path.exists(pkgname) == True:
+        # Todo: 这里可以针对已经存在的包名做一个备份处理, 让用户体验感提升!
         # print("[警告]: 指定的包名工程已经存在, 请重新输入包名!")
         sys.exit(0)
 
-    # os.mkdir(args.pkgname) 
-    # os.chdir(args.pkgname) 
+    # 1. 克隆pkg模板仓库, 并修改名称为需要创建的包名(递归克隆)
+    os.system('git clone --progress --recursive ' + rtthread_pkg_example + " " + pkgname) 
+    os.chdir(pkgname) 
 
-    # 1. 克隆pkg模板仓库, 并修改名称为需要创建的包名
-    cmd = 'git clone --no-tags ' + rtthread_pkg_example_repo + " " + args.pkgname
-    os.system(cmd) 
-    os.chdir(args.pkgname) 
-
-    # 删除模板仓库中的.git目录, Todo: 这里可能在Mac和linux有问题
+    # 删除模板仓库中的.git目录, 
     git_removepath = os.path.join(os.getcwd(), '.git')
-    os.system('attrib -r ' + git_removepath + '\\*.* /s')
+
+    if platform.system() == 'Windows':
+        print("Windows platform") 
+        os.system('attrib -r ' + git_removepath + '\\*.* /s') # 递归修改windows下面的只读文件为可读属性
+    elif platform.system() == 'Linux': 
+        print("Linux platform") # Todo
+    else:
+        print("Other platform") # Todo
+    
     shutil.rmtree(git_removepath)
 
     # 2. 初始化仓库
-    cmd = 'git init' 
-    os.system(cmd) 
+    os.system('git init') 
 
     # 3. 添加子模块
-    cmd = 'git submodule add ' + args.url
-    os.system(cmd) 
+    os.system('git submodule add ' + url) 
 
     # 4. Todo: 提交commit为第一版本
-    cmd = 'git add -A'
-    os.system(cmd) 
-    cmd = 'git commit -m "Quick build ' + args.pkgname + 'pkg for rt-thread."'
-    print(cmd)
-    os.system(cmd) 
+    os.system('git add -A') 
+    os.system('git commit -m "Quick build ' + pkgname + 'pkg for rt-thread by buildpkg toolkits!"') 
+    
+# def buildpkg_update_license()
+#    # Todo
+
+# 生成许可证文件
+def buildpkg_make_license(license): 
+    # Todo
+    print("生成许可证中...")
+
+# scons脚本文件头
+sconscript_head = '''import os
+from building import * 
+
+# get current dir path
+cwd = GetCurrentDir()
+
+# init src and inc vars
+src = []
+inc = []
+
+'''
+
+# scons脚本文件末尾
+sconscript_tail = '''
+# add group to IDE project
+objs = DefineGroup(name + '-' + version, src, depend = ['PKG_USING_' + name.upper()], CPPPATH = inc)
+
+# traversal subscript
+list = os.listdir(cwd)
+if GetDepend('PKG_USING_' + name.upper()):
+    for d in list:
+        path = os.path.join(cwd, d)
+        if os.path.isfile(os.path.join(path, 'SConscript')):
+            objs = objs + SConscript(os.path.join(d, 'SConscript'))
+
+Return('objs') 
+'''
+
+# 生成scons脚本
+def buildpkg_make_scons(pkgname, version):
+    print("生成scons脚本中...") 
+
+    #os.chdir(pkgname) 
+
+    with open('Sconsript', 'w') as f:
+        f.write(sconscript_head)
+        f.write('name    = \'' + pkgname + '\'\n')
+        f.write('version = \'' + version + '\'\n')
+
+        # Todo: 添加
+
+        f.write(sconscript_tail)
+
+    print("生成scons脚本成功!") 
 
 if __name__ == '__main__':
-
-    # 解析buildpkg输出参数
     args = parser.parse_args() 
 
-    # 打印输入参数字典 
-    # print("[提示]: 用户输入的参数列表: " + str(args))
+    # 1. 生成工程 
+    # 2. 生成许可证 
+    buildpkg_make_project(args.pkgname, args.url) 
+    buildpkg_make_license(args.license) 
+    buildpkg_make_scons(args.pkgname, args.version)
 
-    # 创建工程
-    buildpkg_mkdir(args) 
+# inc = inc + [cwd + "/include"]
+# inc = inc + [cwd + "/include/id3v2lib"]
+# src = src + Glob('./id3v2lib/src/*.c') 
